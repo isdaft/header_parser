9b226724ca17800eabaf2c87281c43e162e1b221
isdaft <ben.ashby2@gmail.com>
2016-01-03 22:25:01 +0000

    final commit

--- app/apie/whoami.js
+++ app/apie/whoami.js
@@ -2,7 +2,16 @@
 module.exports = function(app){
 	app.get('/', function(request, response){
 		var headersObj = request.headers;
-		response.send(JSON.stringify(headersObj));
+		//response.writeHead(200, {'Content-Type': 'application/json' });
+		response.setHeader('Content-Type', 'application/json');
+		var ipaddress = request.headers["x-forwarded-for"];
+		var language = request.headers["accept-language"];
+		var language = language.split(',')[0];
+		var software = request.headers["user-agent"];
+		var software = software.split('(')[1];
+		var software = software.split(')')[0];
+		var out = {"ipaddress": ipaddress, "language": language, "software": software};	
+		response.send(JSON.stringify(out));
 	});
 }
 
